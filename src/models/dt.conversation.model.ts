bb2c50cac82c06f6e3fb4302127a3cdb0b61f0af
Felix Miguel Galpao <121932935+probablyFelix@users.noreply.github.com>
2026-02-09 14:09:52 +0800

    Rename model files and add two new models
    Rename many model files to use dot-separated filenames (e.g. btConversation*.model.ts -> bt.conversation*.model.ts, dt*, fs*, ps*, sovereignFutureSignal, userAuth, flightTicket, terminalEcho) while preserving content (most changes are 99% similarity). Add new TerminalEchoReply model (TTerminalEchoReply, TTerminalEchoReplyUpdateOptions, MTerminalEchoReply) and a Todo model (TTodo, TTodoUpdateOptions, MOrganization) in src/models. Also minor file endings/whitespace adjustments. Note: the new Todo class is named MOrganization in the added file.

--- src/models/dt.conversation.model.ts
+++ src/models/dt.conversation.model.ts
@@ -1,21 +1,21 @@
 import { ObjectId } from "mongodb";
 
-export type TPsConversation = {
+export type TDtConversation = {
   _id?: ObjectId;
   createdAt?: Date;
   updatedAt?: Date;
 };
 
-export type TPsConversationUpdateOptions = {
+export type TDtConversationUpdateOptions = {
   _id?: ObjectId | string;
 };
 
-export class MPsConversation implements Partial<TPsConversation> {
+export class MDtConversation implements Partial<TDtConversation> {
   _id?: ObjectId;
   createdAt?: Date;
   updatedAt?: Date;
 
-  constructor({ _id = new ObjectId(), createdAt = new Date(), updatedAt } = {} as TPsConversation) {
+  constructor({ _id = new ObjectId(), createdAt = new Date(), updatedAt } = {} as TDtConversation) {
     this._id = _id;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
